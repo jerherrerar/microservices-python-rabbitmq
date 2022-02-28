23f0b4dda7b5c56274e876d93b46a2cf4310f14d
Jerson Herrera Rivera <jerson@inka-labs.com>
2022-02-27 19:21:05 -0500

    add flask application

--- README.md
+++ README.md
@@ -4,4 +4,24 @@ Microservices Python: architecture based in events with RabbitMQ
 
 
 guide: https://www.youtube.com/watch?v=0iB5IPoTDts
-21:30
+51:45
+
+
+cd JERSON/myRepo/microservices-python-rabbitmq/
+code .
+source python-env/bin/activate
+sudo docker-compose up
+
+
+docker-compose exec backend sh
+python manage.py makemigrations
+python manage.py migrate
+
+
+cd main/
+sudo docker-compose up
+
+docker-compose exec backend sh
+python manager.py db init
+python manager.py db migrate
+python manager.py db upgrade
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
+import random
 
-# Create your views here.
+from rest_framework import views, viewsets, status
+from rest_framework.response import Response
+
+from .models import Product, User
+from .serializers import ProductSerializer
+
+class ProductViewSet(viewsets.ViewSet):
+
+    def list(self, request):
+        products = Product.objects.all()
+        return Response(ProductSerializer(products, many=True).data)
+
+    def create(self, request):
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    def retrieve(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        return Response(ProductSerializer(product).data)
+
+    def update(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        serializer = ProductSerializer(instance=product, data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+
+    def destroy(self, request, pk=None):
+        product = Product.objects.get(pk=pk)
+        product.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserAPIView(views.APIView):
+    def get(self, _):
+        users = User.objects.all()
+        user = random.choice(users)
+        return Response({
+            'id': user.pk,
+        })
